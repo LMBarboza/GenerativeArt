b905804944cc2b5b53ec66a8493b2111646e42bb
LMBarboza <leon.uchoa@gmail.com>
2024-08-29 00:24:45 -0300

    update main function

--- fractals/julia/src/main.cpp
+++ fractals/julia/src/main.cpp
@@ -1,37 +1,70 @@
 #define GLFW_INCLUDE_NONE
+#include "../include/EBO.hpp"
+#include "../include/VAO.hpp"
+#include "../include/VBO.hpp"
+#include "../include/shader.hpp"
 #include <GLFW/glfw3.h>
+#include <cmath>
 #include <glad/glad.h>
 #include <iostream>
 
-int main(void)
+GLfloat vertices[] = {0.0f,      0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
+                      -0.5f / 2, 0.5f * float(sqrt(3)) / 6,     0.0f,
+                      0.5f / 2,  0.5f * float(sqrt(3)) / 6,     0.0f,
+                      0.0f,      -0.5f * float(sqrt(3)) / 3,    0.0f};
 
-{
+GLuint indices[] = {0, 3, 5, 3, 2, 4, 5, 4, 1};
+
+int main() {
   glfwInit();
+
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
+  GLFWwindow *window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
   if (window == NULL) {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
     return -1;
   }
-  glfwMakeContextCurrent(window);
 
+  glfwMakeContextCurrent(window);
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     std::cout << "Failed to initialize GLAD" << std::endl;
     return -1;
   }
-  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
-  glViewport(0, 0, 800, 600);
+
+  glViewport(0, 0, 800, 800);
+
+  Shader shaderProgram("../shaders/shader.vert", "../shaders/shader.frag");
+
+  VAO VAO1;
+  VAO1.Bind();
+
+  VBO VBO1(vertices, sizeof(vertices));
+  EBO EBO1(indices, sizeof(indices));
+
+  VAO1.LinkVBO(VBO1, 0);
+  VAO1.Unbind();
+  VBO1.Unbind();
+  EBO1.Unbind();
 
   while (!glfwWindowShouldClose(window)) {
-    glfwSwapBuffers(window);
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+    shaderProgram.Activate();
+    VAO1.Bind();
+    glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+    glfwSwapBuffers(window);
     glfwPollEvents();
   }
+
+  VAO1.Delete();
+  VBO1.Delete();
+  EBO1.Delete();
+  shaderProgram.Delete();
+  glfwDestroyWindow(window);
   glfwTerminate();
   return 0;
 }
